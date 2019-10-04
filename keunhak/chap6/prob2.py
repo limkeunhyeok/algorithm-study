a854158ecdc9a4d48425a5368f656ee808e0539a
Keunhak Lim <limkeunhak@gmail.com>
2019-10-05 00:34:19 +0900

    lkh-6-2

--- keunhak/chap6/prob2.py
+++ keunhak/chap6/prob2.py
@@ -1,6 +1,67 @@
+import copy
+
+shapes = [
+    ['##', 
+     '#.'],
+    ['##', 
+     '.#'],
+    ['#.', 
+     '##'],
+    ['.#', 
+     '##']]
+
 def solution(n, m, boardMap):
+    if getNumDot(boardMap) == 0:
+        return 0
+    if getNumDot(boardMap) % 3 != 0:
+        return 0
+
     return makeFullBoard(n, m, boardMap)
 
+# 시간이 많이 걸려 프로그램 수정이 필요함
 def makeFullBoard(n, m, boardMap):
-    return 0
+    if getNumDot(boardMap) == 0:
+        return 1
+
+    pos = getFirstEmptyPos(n, m, boardMap)
+    
+    num = 0
+    for shape in shapes:
+        if canPlace(pos[0], pos[1], shape, boardMap):
+            boardMapCopy = copy.deepcopy(boardMap)
+            boardMapCopy[pos[0]][pos[1]] = '#' if shape[0][0] == '#' else boardMapCopy[pos[0]][pos[1]]
+            boardMapCopy[pos[0]][pos[1] + 1] = '#' if shape[0][1] == '#' else boardMapCopy[pos[0]][pos[1] + 1]
+            boardMapCopy[pos[0] + 1][pos[1]] = '#' if shape[1][0] == '#' else boardMapCopy[pos[0] + 1][pos[1]]
+            boardMapCopy[pos[0] + 1][pos[1] + 1] = '#' if shape[1][1] == '#' else boardMapCopy[pos[0] + 1][pos[1] + 1]
+            num += makeFullBoard(n, m, boardMapCopy)
+
+        if pos[1] > 0 and canPlace(pos[0], pos[1] - 1, shape, boardMap):
+            boardMapCopy = copy.deepcopy(boardMap)
+            boardMapCopy[pos[0]][pos[1] - 1] = '#' if shape[0][0] == '#' else boardMapCopy[pos[0]][pos[1] - 1]
+            boardMapCopy[pos[0]][pos[1]] = '#' if shape[0][1] == '#' else boardMapCopy[pos[0]][pos[1]]
+            boardMapCopy[pos[0] + 1][pos[1] - 1] = '#' if shape[1][0] == '#' else boardMapCopy[pos[0] + 1][pos[1] - 1]
+            boardMapCopy[pos[0] + 1][pos[1]] = '#' if shape[1][1] == '#' else boardMapCopy[pos[0] + 1][pos[1]]
+            num += makeFullBoard(n, m, boardMapCopy)
+    return num
+
+def getFirstEmptyPos(n, m, boardMap):
+    for i in range(n - 1):
+        for j in range(m - 1):
+            if boardMap[i][j] == '.':
+                return (i, j)
+
+def getNumDot(boardMap):
+    num = 0
+    for line in boardMap:
+        num += line.count('.')
+    return num
+
+def canPlace(x, y, shape, boardMap):
+    canPlace = True
+    if boardMap[x][y] == '#' and shape[0][0] == '#': canPlace = False
+    if boardMap[x][y + 1] == '#' and shape[0][1] == '#': canPlace = False
+    if boardMap[x + 1][y] == '#' and shape[1][0] == '#': canPlace = False
+    if boardMap[x + 1][y + 1] == '#' and shape[1][1] == '#': canPlace = False
+
+    return canPlace
 
--- keunhak/chap6/tests.py
+++ keunhak/chap6/tests.py
@@ -1,8 +1,8 @@
 import unittest
-import boggle, prob1
+import boggle, prob1, prob2
 
 class Chap6ProbTest(unittest.TestCase):
-    # Prob1 tests
+    # boggle tests
     def test_boggle_case_1(self):
         result = boggle.solution(1, 1, 'pretty')
         self.assertEqual(result, True)
@@ -22,19 +22,53 @@ class Chap6ProbTest(unittest.TestCase):
         result = boggle.solution(0, 0, 'upr')
         self.assertEqual(result, True)
 
-    # Prob2 tests
-    def test_prob2_case_1(self):
+    # Prob1 tests
+    def test_prob1_case_1(self):
         result = prob1.solution(2, 1, [(0, 1)])
         self.assertEqual(result, 1)
-    def test_prob2_case_2(self):
+    def test_prob1_case_2(self):
         result = prob1.solution(4, 6, [(0, 1), (1, 2), (2, 3), (3, 0), (0, 2), (1, 3)])
         self.assertEqual(result, 3)
-    def test_prob2_case_3(self):
+    def test_prob1_case_3(self):
         result = prob1.solution(6, 10, [(0, 1), (0, 2), (1, 2), (1, 3), (1, 4), (2, 3), (2, 4), (3, 4), (3, 5), (4, 5)])
         self.assertEqual(result, 4)
-    def test_prob2_case_4(self):
+    def test_prob1_case_4(self):
         result = prob1.solution(4, 1, [(0, 1)])
         self.assertEqual(result, 0)
+
+    # Prob1 tests
+    def test_prob2_case_1(self):
+        result = prob2.solution(3, 7, [
+            ['#', '.', '.', '.', '.', '.', '#'],
+            ['#', '.', '.', '.', '.', '.', '#'],
+            ['#', '#', '.', '.', '.', '#', '#']
+            ])
+        self.assertEqual(result, 0)
+    def test_prob2_case_2(self):
+        result = prob2.solution(3, 7, [
+            ['#', '.', '.', '.', '.', '.', '#'],
+            ['#', '.', '.', '.', '.', '.', '#'],
+            ['#', '#', '.', '.', '#', '#', '#']
+            ])
+        self.assertEqual(result, 2)
+    def test_prob2_case_3(self):
+        result = prob2.solution(2, 3, [
+            ['#', '.', '#'],
+            ['#', '.', '.']
+            ])
+        self.assertEqual(result, 1)
+    def test_prob2_case_4(self):
+        result = prob2.solution(8, 10, [
+            ['#', '#', '#', '#', '#', '#', '#', '#', '#', '#'],
+            ['#', '.', '.', '.', '.', '.', '.', '.', '.', '#'],
+            ['#', '.', '.', '.', '.', '.', '.', '.', '.', '#'],
+            ['#', '.', '.', '.', '.', '.', '.', '.', '.', '#'],
+            ['#', '.', '.', '.', '.', '.', '.', '.', '.', '#'],
+            ['#', '.', '.', '.', '.', '.', '.', '.', '.', '#'],
+            ['#', '.', '.', '.', '.', '.', '.', '.', '.', '#'],
+            ['#', '#', '#', '#', '#', '#', '#', '#', '#', '#']
+            ])
+        self.assertEqual(result, 1514)
 
 if __name__ == '__main__':
     unittest.main()
