5ff33d9535aaa91b70be43fac74b5cd9e666dc7d
Keunhak Lim <limkeunhak@gmail.com>
2019-10-16 00:40:09 +0900

    lkh-7-1 교재 방식 풀이

--- keunhak/chap7/prob1-1.py
+++
@@ -1,6 +0,0 @@
-# solution 2 (use divide & conquer)
-def solution(n, trees):
-    return turnOver(tree)
-
-def turnOver(tree):
-    return 'x'
--- keunhak/chap7/tests.py
+++ keunhak/chap7/tests.py
@@ -1,5 +1,5 @@
 import unittest
-import prob1
+import prob1, prob1_2
 
 class Chap7ProbTests(unittest.TestCase):
     def test_prob1_case_1(self):
@@ -13,6 +13,18 @@ class Chap7ProbTests(unittest.TestCase):
         self.assertEqual(result, 'xxbwwbbbw')
     def test_prob1_case_4(self):
         result = prob1.solution('xxwwwbxwxwbbbwwxxxwwbbbwwwwbb')
+        self.assertEqual(result, 'xxwbxwwxbbwwbwbxwbwwxwwwxbbwb')
+    def test_prob1_2_case_1(self):
+        result = prob1_2.solution('w')
+        self.assertEqual(result, 'w')
+    def test_prob1_2_case_2(self):
+        result = prob1_2.solution('xbwwb')
+        self.assertEqual(result, 'xwbbw')
+    def test_prob1_2_case_3(self):
+        result = prob1_2.solution('xbwxwbbwb')
+        self.assertEqual(result, 'xxbwwbbbw')
+    def test_prob1_2_case_4(self):
+        result = prob1_2.solution('xxwwwbxwxwbbbwwxxxwwbbbwwwwbb')
         self.assertEqual(result, 'xxwbxwwxbbwwbwbxwbwwxwwwxbbwb')
 
 if __name__ == '__main__':
