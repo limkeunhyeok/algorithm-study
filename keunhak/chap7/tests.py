5c6a000626a2bea89e3bbc1720413ce916c1e50f
Keunhak Lim <limkeunhak@gmail.com>
2019-10-16 02:06:54 +0900

    lkh-7-2 brute force (변경 필요)

--- keunhak/chap7/tests.py
+++ keunhak/chap7/tests.py
@@ -1,5 +1,5 @@
 import unittest
-import prob1, prob1_2
+import prob1, prob1_2, prob2
 
 class Chap7ProbTests(unittest.TestCase):
     def test_prob1_case_1(self):
@@ -14,6 +14,7 @@ class Chap7ProbTests(unittest.TestCase):
     def test_prob1_case_4(self):
         result = prob1.solution('xxwwwbxwxwbbbwwxxxwwbbbwwwwbb')
         self.assertEqual(result, 'xxwbxwwxbbwwbwbxwbwwxwwwxbbwb')
+
     def test_prob1_2_case_1(self):
         result = prob1_2.solution('w')
         self.assertEqual(result, 'w')
@@ -26,6 +27,22 @@ class Chap7ProbTests(unittest.TestCase):
     def test_prob1_2_case_4(self):
         result = prob1_2.solution('xxwwwbxwxwbbbwwxxxwwbbbwwwwbb')
         self.assertEqual(result, 'xxwbxwwxbbwwbwbxwbwwxwwwxbbwb')
+
+    def test_prob2_case_1(self):
+        result = prob2.solution([7, 1, 5, 9, 6, 7, 3])
+        self.assertEqual(result, 20)
+    def test_prob2_case_2(self):
+        result = prob2.solution([1, 4, 4, 4, 4, 1, 1])
+        self.assertEqual(result, 16)
+    def test_prob2_case_3(self):
+        result = prob2.solution([1, 8, 1, 1])
+        self.assertEqual(result, 8)
+    def test_prob2_case_4(self):
+        result = prob2.solution([10000, 10000, 10000 ])
+        self.assertEqual(result, 30000)
+    def test_prob2_case_5(self):
+        result = prob2.solution([10000 for T in range(20000)])
+        self.assertEqual(result, 200000000)
 
 if __name__ == '__main__':
     unittest.main()
